ce54de741e07bfc0bdcd0d321cba8c9faaa4e51e
Caden Chan <caadenchan@gmail.com>
2024-04-26 18:55:36 -0400

    basic player mechanics

---
+++ scripts/player.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+
+const VEL = 230.0
+const ACC = 1600.0
+const FRIC = 2000.0
+const JUMP_VEL = -360.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+	apply_graivity(delta)
+	handle_jump()
+	
+	var direction = Input.get_axis("left", "right")  # if left, -1; if right, +1, else 0
+	
+	handle_movement(delta, direction)
+	
+	update_animations(direction)
+	move_and_slide()  # ** automatically applies "delta"
+
+func apply_graivity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VEL  # no delta bc move_and_slide()
+	else:
+		if Input.is_action_just_released("jump") and velocity.y < JUMP_VEL/3:
+			velocity.y = JUMP_VEL / 3
+
+func handle_movement(delta, direction):
+	if direction:
+		velocity.x = move_toward(velocity.x, VEL*direction, ACC*delta)  # player acceleration
+	else:
+		velocity.x = move_toward(velocity.x, 0, FRIC*delta)  # player friction
+	
+func update_animations(direction):
+	if direction == 0:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.flip_h = (direction < 0)
+		if not is_on_floor():
+			animated_sprite_2d.play("jump")
+		else:
+			animated_sprite_2d.play("run")
+		
--- scripts/world.gd
+++ scripts/world.gd
@@ -3,10 +3,13 @@ extends Node2D
 @onready var flatland_collider = $Flatland/FlatlandCollider
 @onready var flatland_polygon = $Flatland/FlatlandCollider/FlatlandPolygon
 
+@onready var testland_collider = $Testland/TestlandCollider
+@onready var testland_polygon = $Testland/TestlandCollider/TestlandPolygon
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	flatland_polygon.polygon = flatland_collider.polygon
+	testland_polygon.polygon = testland_collider.polygon
 
 
 
