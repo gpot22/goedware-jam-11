728fefb4aa2677d1a3a2654789c764f9cd26c150
Caden Chan <caadenchan@gmail.com>
2024-04-26 20:26:30 -0400

    black bg

--- scripts/world.gd
+++ scripts/world.gd
@@ -8,6 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	RenderingServer.set_default_clear_color(Color.BLACK)
 	flatland_polygon.polygon = flatland_collider.polygon
 	testland_polygon.polygon = testland_collider.polygon
 
