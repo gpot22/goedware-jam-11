b1d297fb98b95b6a62cd61343c79f70235ffb611
Jaylen <jaylencheung@gmail.com>
2024-04-26 20:35:54 -0400

    sidescrolling

--- scripts/world.gd
+++ scripts/world.gd
@@ -6,11 +6,16 @@ extends Node2D
 @onready var testland_collider = $Testland/TestlandCollider
 @onready var testland_polygon = $Testland/TestlandCollider/TestlandPolygon
 
+@onready var camera_2d = $Camera2D
+@onready var player = $Player
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
 	flatland_polygon.polygon = flatland_collider.polygon
 	testland_polygon.polygon = testland_collider.polygon
 
+func _process(delta):
+	camera_2d.set_position(Vector2(player.get_position().x, player.get_position().y-35))
 
 
